801958a635ed262aa295882f9b1461ad028ec27c
afsharm (l) <afshar.mohebbi@gmail.com>
2011-09-13 13:54:40 +0430

    date time helper unit test passed

--- Source/Fardis.Test/DateTimeHelperTest.cs
+++ Source/Fardis.Test/DateTimeHelperTest.cs
@@ -19,13 +19,23 @@ namespace Fardis.Test
         [Test]
         public void ConvertToPersianDateTest()
         {
-            Assert.Fail();
+            Assert.AreEqual("1390/6/22", dateTimeHelper.ConvertToPersianDate(Convert.ToDateTime("2011/09/13")));
+            Assert.AreEqual(string.Empty, dateTimeHelper.ConvertToPersianDate(null));
+            Assert.AreEqual("1390/1/1", dateTimeHelper.ConvertToPersianDate(Convert.ToDateTime("2011/03/21")));
+            Assert.AreEqual("1389/12/29", dateTimeHelper.ConvertToPersianDate(Convert.ToDateTime("2011/03/20")));
+            Assert.AreEqual("1387/12/30", dateTimeHelper.ConvertToPersianDate(Convert.ToDateTime("2009/03/20")));
+            Assert.AreEqual("1388/1/1", dateTimeHelper.ConvertToPersianDate(Convert.ToDateTime("2009/03/21")));
         }
 
         [Test]
         public void ConvertToPersianDatePersianDigitTest()
         {
-            Assert.Fail();
+            Assert.AreEqual("۱۳۹۰/۶/۲۲", dateTimeHelper.ConvertToPersianDatePersianDigit(Convert.ToDateTime("2011/09/13")));
+            Assert.AreEqual(string.Empty, dateTimeHelper.ConvertToPersianDatePersianDigit(null));
+            Assert.AreEqual("۱۳۹۰/۱/۱", dateTimeHelper.ConvertToPersianDatePersianDigit(Convert.ToDateTime("2011/03/21")));
+            Assert.AreEqual("۱۳۸۹/۱۲/۲۹", dateTimeHelper.ConvertToPersianDatePersianDigit(Convert.ToDateTime("2011/03/20")));
+            Assert.AreEqual("۱۳۸۷/۱۲/۳۰", dateTimeHelper.ConvertToPersianDatePersianDigit(Convert.ToDateTime("2009/03/20")));
+            Assert.AreEqual("۱۳۸۸/۱/۱", dateTimeHelper.ConvertToPersianDatePersianDigit(Convert.ToDateTime("2009/03/21")));
         }
     }
 }
